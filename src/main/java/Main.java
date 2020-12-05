151a1924f46af2fcffe6fb4e4e27ed6dad351e89
irynabolyukh <iryna.boliukh@ukma.edu.ua>
2020-12-05 22:31:54 +0200

    initial

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+//import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
+public class Main {
+
+   public static void main(String[] args) throws IOException {
+
+      HttpClient client = HttpClient.newHttpClient();
+      HttpRequest request = HttpRequest.newBuilder()
+              .uri(URI.create("http://localhost:8081/game"))
+              .build();
+      client.sendAsync(request, BodyHandlers.ofString())
+              .thenApply(HttpResponse::body)
+              .thenAccept(System.out::println)
+              .join();
+
+   }
+
+}
